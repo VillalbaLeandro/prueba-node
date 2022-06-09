183e98efb12350c777ac1fe992739e5680202e11
VillalbaLeandro <exemprary@gmail.com>
2022-06-09 12:29:33 -0300

    prueba2

---
+++ calculadora.js
@@ -0,0 +1,6 @@
+const dividir = require("./dividir.js");
+const multiplicar = require("./multiplicar.js");
+const restar = require("./restar.js");
+const sumar = require("./sumar.js");
+
+console.log(dividir(10,2));
---
+++ dividir.js
@@ -0,0 +1,11 @@
+function dividir(num1, num2) {
+    if (num2 == 0) {
+        return "no se puede dividir por cero"
+    }
+    return num1 / num2;
+
+}
+
+module.exports = dividir;
+
+
